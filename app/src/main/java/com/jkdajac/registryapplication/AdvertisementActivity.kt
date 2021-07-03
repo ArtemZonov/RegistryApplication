02cfc1415cb7b2339639d0873e153780f1c34190
ARTEM <jkda@ukr.net>
2021-07-03 18:49:15 +0300

    added advertisement Activity

--- app/src/main/java/com/jkdajac/registryapplication/AdvertisementActivity.kt
+++ app/src/main/java/com/jkdajac/registryapplication/AdvertisementActivity.kt
@@ -3,17 +3,15 @@ package com.jkdajac.registryapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_advertisement.*
 
-class MainActivity : AppCompatActivity() {
+class AdvertisementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        btGoToAdvertise.setOnClickListener {
+        setContentView(R.layout.activity_advertisement)
+        ivBack.setOnClickListener {
             var intent = Intent(this, AdvertisementActivity::class.java)
             startActivity(intent)
-
         }
     }
 }
